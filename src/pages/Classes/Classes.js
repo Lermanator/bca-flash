8173aeea4a33685ec26e7d05422e7014786096d1
J-Tell <fairyq007@hotmail.com>
2024-03-07 22:45:38 -0500

    descriptions for the CS class is done

--- src/pages/Classes/Classes.js
+++ src/pages/Classes/Classes.js
@@ -26,7 +26,14 @@ function Classes() {
 
               <Row className="classRow">
                 <h1>Computer Science</h1>
-                <ClassCard className="Code a Game!" desc="coding a game fr"></ClassCard>
+                {/* cut down on some of the descriptions */}
+                <ClassCard className="Code a Game!" desc="Students will code their own choose-your-own adventure game while learn how to use HTML and CSS. This class will teach students
+                about both the functionality and the design aspects of website development and will offer students a hands-on opportunity to learn more about coding."></ClassCard>
+                <ClassCard className="Python Programming" desc="Students will learn the basics of python programming
+                by codng a game of Rock, Paper, Scissors. After making this game, they will also learn to code another game
+                based on their skill level. These games will have the students break up into seperate groups to create different games."></ClassCard>
+                <ClassCard className="Machine Learning Basic" desc="Students will learn about three different types of machine learning, then
+                implement what they learn to create an AI model. Students will learn how to create a K-Nearest Neighbor model, as well as learning how to upload a dataset."></ClassCard>
               </Row>
 
               <Row className="classRow">
